e8cb0bfed52b0e07998ccbee5e2a6cbef77f36cf
Almaz <63015341+Morr231@users.noreply.github.com>
2022-09-17 15:56:03 +0600

    added sass and userpage

--- src/components/MyRoutes/MyRoutes.js
+++ src/components/MyRoutes/MyRoutes.js
@@ -1,33 +1,41 @@
-import {Route, Routes} from "react-router";
-import {MainPage} from "../../pages/MainPage/MainPage"
+import { Route, Routes } from "react-router";
+import { MainPage } from "../../pages/MainPage/MainPage";
+import Userpage from "../userpage/userpage";
 
 export const MyRoutes = () => {
     const routes = [
         {
-          path: '/',
-          component: <MainPage/>
+            path: "/",
+            component: <MainPage />,
         },
         {
-            path: '/products',
-            component: <MainPage/>
+            path: "/products",
+            component: <MainPage />,
         },
         {
-            path: '/signUp',
-            component: <MainPage/>
+            path: "/signUp",
+            component: <MainPage />,
         },
         {
-            path: '/signIn',
-            component: <MainPage/>
+            path: "/signIn",
+            component: <MainPage />,
         },
-    ]
+        {
+            path: "/account",
+            component: <Userpage />,
+        },
+    ];
 
-    return(
+    return (
         <Routes>
-            {
-                routes.length > 0 && routes.map(el => (
-                    <Route key={el.path} path={el.path} element={el.component}/>
-                ))
-            }
+            {routes.length > 0 &&
+                routes.map((el) => (
+                    <Route
+                        key={el.path}
+                        path={el.path}
+                        element={el.component}
+                    />
+                ))}
         </Routes>
-    )
-}
+    );
+};
---
+++ src/components/userpage/userpage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./userpage-sass/userpage.sass";
+
+const Userpage = () => {
+    return <div className="userpage">Userpage</div>;
+};
+
+export default Userpage;
