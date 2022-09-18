924c29acd37e7663cf5c66deea52ef481227e490
1August <boktaban@gmail.com>
2022-09-18 16:51:34 +0600

    Revert "modal, add new product"
    This reverts commit 4226152ba41c8bee9d459362f23304d7908eac33.

--- src/redux/asyncActions/fetchUsers.js
+++ src/redux/asyncActions/fetchUsers.js
@@ -1,10 +1,26 @@
 import axios from "axios"
-import {addUserAction} from "../usersReducer"
+import {addManyUsersAction, addUserAction} from "../usersReducer"
 import {setCredentialsAction} from "../authReducer"
 
 import {decodeToken} from 'react-jwt'
 
+export const fetchManyUsers = () =>
+    dispatch =>
+        axios.get('https://reqres.in/api/users?page=1')
+            .then(res => dispatch(addManyUsersAction(res.data.data)))
+
 /**
+ *
+ * {
+ *     "username": "effuone",
+ *     "firstName": "Alibek",
+ *     "lastName": "Seitov",
+ *     "image": "image.png",
+ *     "locationId": 5,
+ *     "email": "albkfil@gmail.com",
+ *     "phoneNumber": "87019867333",
+ *     "password": "1z2x3c4v5B*"
+ * }
  *
  * @param payload
  * @returns {function(*): Promise<AxiosResponse<any>>}
@@ -18,8 +34,34 @@ export const signUp = payload =>
                 }
                 dispatch(addUserAction(payload))
             })
+// axios.post('https://reqres.in/api/register', {
+//     username: payload.email,
+//     email: payload.email,
+//     password: payload.password
+// })
+//     .then(res => {
+//         if (res.status !== 200) return
+//         dispatch(addUserAction({...payload, id: res.data.id}))
+//         dispatch(setCredentialsAction({user: {...payload, id: res.data.id}, token: res.data.token}))
+//
+//         const user = {
+//             id: res.data.id,
+//             email: payload.email,
+//             first_name: payload.first_name,
+//             last_name: payload.last_name,
+//             token: res.data.token
+//         }
+//         localStorage.setItem('user', JSON.stringify(user))
+//     })
+
 
 /**
+ *
+ * {
+ *     "username":"effuone",
+ *     "password": "1z2x3c4v5B*"
+ * }
+ *
  * @param payload
  * @returns {function(*): Promise<AxiosResponse<any>>}
  */
@@ -30,6 +72,10 @@ export const signIn = payload =>
                 if (res.status !== 200) {
                     throw new Error('Can not login. Status is wrong.')
                 }
+                /**
+                 * TODO:
+                 * Need to get __UserID__
+                 */
 
                 const data = decodeToken(res.data.token)
 
