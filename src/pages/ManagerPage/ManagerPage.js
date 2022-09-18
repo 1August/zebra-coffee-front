3ef2a97df12071d33b33259b6f55b02c942340e3
1August <boktaban@gmail.com>
2022-09-18 17:57:20 +0600

    Update ManagerPage.js

--- src/pages/ManagerPage/ManagerPage.js
+++ src/pages/ManagerPage/ManagerPage.js
@@ -60,7 +60,7 @@ export const ManagerPage = () => {
 
     useEffect(() => {
         const getProducts = async () => {
-            const res = await axios.get("https://zebra-hackathon.herokuapp.com/api/products?page=1&limit=15");
+            const res = await axios.get("https://zebra-hackathon.herokuapp.com/api/products?page=1&limit=40");
             setProducts(res.data.results);
             // console.log(res.data.results)
         };
