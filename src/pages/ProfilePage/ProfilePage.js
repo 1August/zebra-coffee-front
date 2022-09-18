3acb5814f0a7d04d3f91248595cdc820909d4632
1August <boktaban@gmail.com>
2022-09-18 17:08:07 +0600

    Revert "Merge branch 'Maksat'"
    This reverts commit 632b2b552558102a37556f00f62bd0a6f711636e, reversing
    changes made to 924c29acd37e7663cf5c66deea52ef481227e490.

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,120 +1,69 @@
-import "./ProfilePage.scss";
-import { MdOutlineAccountCircle } from "react-icons/md";
-import { useDispatch, useSelector } from "react-redux";
-import { logOutAction } from "../../redux/authReducer";
-import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { Input } from "../../UI/Input/Input";
-import axios from "axios";
+import './ProfilePage.scss'
+import {MdOutlineAccountCircle} from "react-icons/md";
+import {useDispatch, useSelector} from "react-redux";
+import {logOutAction} from "../../redux/authReducer";
+import {NavLink} from "react-router-dom";
+import {useState} from "react";
+import {Input} from "../../UI/Input/Input";
 
 export const ProfilePage = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch()
 
-    const user = useSelector((state) => state.auth.user);
+    const user = useSelector(state => state.auth.user)
 
-    const handleSignOutClick = (e) => {
-        e.preventDefault();
-        dispatch(logOutAction());
-    };
-
-    const [searchQuery, setSearchQuery] = useState("");
-
-    const handleSearchQueryChange = (e) => {
-        setSearchQuery(e.target.value);
-    };
-
-    const [orders, setOrders] = useState(null);
-    const [allOrders, setAllOrders] = useState(null);
-    const [activeButtons, setActiveButtons] = useState(null);
-
-    useEffect(() => {
-        const getOrders = async () => {
-            const result = await axios.get(
-                `https://zebra-hackathon.herokuapp.com/api/orders/user/${user.id}`
-            );
-            setOrders(result.data);
-        };
-        getOrders();
-    }, []);
-
-    useEffect(() => {
-        const getAllOrders = async () => {
-            const allOrders = [];
-
-            orders.forEach(async (el) => {
-                const result = await axios.get(
-                    `https://zebra-hackathon.herokuapp.com/api/orders/${el.id}`
-                );
-
-                allOrders.push({
-                    id: result.data.id,
-                    orderItems: result.data.orderItems,
-                });
-            });
-
-            setAllOrders(allOrders);
-        };
-        if (orders) {
-            getAllOrders();
-        }
-    }, [orders]);
-
-    useEffect(() => {
-        if (!activeButtons && orders) {
-            const tempActiveButtons = [];
-            orders.forEach((el) => {
-                tempActiveButtons.push({
-                    id: el.id,
-                    active: false,
-                });
-            });
-            setActiveButtons(tempActiveButtons);
-        }
-    }, [orders]);
-
-    // const orders = [
-    //     {
-    //         statusOrder: 1,
-    //         statusTitle: "Lorem",
-    //         statusDate: Date.now(),
-    //         statusInfo: "See more",
-    //     },
-    //     {
-    //         statusOrder: 2,
-    //         statusTitle: "Lorem",
-    //         statusDate: Date.now(),
-    //         statusInfo: "See more",
-    //     },
-    //     {
-    //         statusOrder: 3,
-    //         statusTitle: "Lorem",
-    //         statusDate: Date.now(),
-    //         statusInfo: "See more",
-    //     },
-    //     {
-    //         statusOrder: 4,
-    //         statusTitle: "Lorem",
-    //         statusDate: Date.now(),
-    //         statusInfo: "See more",
-    //     },
-    //     {
-    //         statusOrder: 5,
-    //         statusTitle: "Lorem",
-    //         statusDate: Date.now(),
-    //         statusInfo: "See more",
-    //     },
-    // ];
-
-    if (!orders) {
-        return <div className="loading">Loading</div>;
+    const handleSignOutClick = e => {
+        e.preventDefault()
+        dispatch(logOutAction())
     }
+
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const handleSearchQueryChange = e => {
+        setSearchQuery(e.target.value)
+    }
+
+    const orders = [
+        {
+            statusOrder: 1,
+            statusTitle: 'Lorem',
+            statusDate: Date.now(),
+            statusInfo: 'See more'
+        },
+        {
+            statusOrder: 2,
+            statusTitle: 'Lorem',
+            statusDate: Date.now(),
+            statusInfo: 'See more'
+        },
+        {
+            statusOrder: 3,
+            statusTitle: 'Lorem',
+            statusDate: Date.now(),
+            statusInfo: 'See more'
+        },
+        {
+            statusOrder: 4,
+            statusTitle: 'Lorem',
+            statusDate: Date.now(),
+            statusInfo: 'See more'
+        },
+        {
+            statusOrder: 5,
+            statusTitle: 'Lorem',
+            statusDate: Date.now(),
+            statusInfo: 'See more'
+        },
+    ]
 
     return (
         <div className="profilePage" id="profilePage">
             <div className="container">
                 <div className="profileInfo">
                     <div className="profileInfo-top">
-                        <img src={user.image} alt="Profile image" />
+                        <img
+                            src={user.image}
+                            alt="Profile image"
+                        />
                     </div>
                     <div className="profileInfo-text">
                         <h1>{`${user?.firstName} ${user?.lastName}`}</h1>
@@ -123,7 +72,7 @@ export const ProfilePage = () => {
                         <p>{user?.location.country}</p>
                         <p>
                             <NavLink
-                                to={"/signOut"}
+                                to={'/signOut'}
                                 onClick={handleSignOutClick}
                             >
                                 SignOut
@@ -147,77 +96,33 @@ export const ProfilePage = () => {
                                 type="search"
                                 value={searchQuery}
                                 onChange={handleSearchQueryChange}
-                                placeholder={"Search"}
+                                placeholder={'Search'}
                             />
                         </div>
                     </div>
                     <div className="statusesList">
-                        <div className="statusItem">
-                            <p className={"statusOrder"}>#</p>
-                            <p className={"statusOrder"}>ID</p>
-                            <h3 className="statusTitle">Адрес</h3>
-                            <h3 className="statusDate">Дата</h3>
-                            <h3 className="statusDate">Действие</h3>
-                        </div>
-
-                        {orders.length > 0 ? (
-                            orders
-                                // .filter((el) =>
-                                //     el.statusTitle
-                                //         .toLowerCase()
-                                //         .includes(searchQuery.toLowerCase())
-                                // )
-                                ?.map((el, index) => {
-                                    const date = new Date(el?.order_date)
-                                        .toDateString()
-                                        .split(" ");
-
-                                    // const actButton = activeButtons.filter(
-                                    //     (button) => button.id === el.id
-                                    // )[0];
-
-                                    return (
-                                        <>
-                                            <div className="statusItem">
-                                                <p className={"statusOrder"}>
-                                                    {index + 1}
-                                                </p>
-                                                <p className={"statusOrder"}>
-                                                    {el?.id}
-                                                </p>
-                                                <h3 className="statusTitle">
-                                                    {el?.street}
-                                                </h3>
-                                                <h3 className="statusDate">
-                                                    {date[2]} {date[1]}{" "}
-                                                    {date[3]}{" "}
-                                                </h3>
-                                                <button
-                                                    className="statusInfo"
-                                                    // onClick={() =>
-                                                    //     activeButtons.filter(
-                                                    //         (button) =>
-                                                    //             button.id ===
-                                                    //             el.id
-                                                    //     )[0]
-                                                    // }
-                                                >
-                                                    <MdOutlineAccountCircle />
-                                                    Подробнее
-                                                </button>
-                                            </div>
-                                            <div className="statusItem">
-                                                123
-                                            </div>
-                                        </>
-                                    );
-                                })
-                        ) : (
-                            <h1>List is empty!</h1>
-                        )}
+                        {
+                            orders.length > 0 ? orders.filter(el => el.statusTitle.toLowerCase().includes(searchQuery.toLowerCase()))?.map(el => (
+                                <div className="statusItem">
+                                    <p className={'statusOrder'}>
+                                        {el?.statusOrder}
+                                    </p>
+                                    <h3 className="statusTitle">
+                                        {el?.statusTitle}
+                                    </h3>
+                                    <h3 className="statusDate">
+                                        {el?.statusDate}
+                                    </h3>
+                                    <button className="statusInfo">
+                                        <MdOutlineAccountCircle/>
+                                        {el?.statusInfo}
+                                    </button>
+                                </div>
+                            )) : <h1>List is empty!</h1>
+                        }
                     </div>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
