65dd038724a3f8d7b6794c8295498f6b1d114307
1August <boktaban@gmail.com>
2022-09-18 16:51:34 +0600

    Revert "modal, add new product"
    This reverts commit 4226152ba41c8bee9d459362f23304d7908eac33.

--- src/components/products/products-comp/product-card.js
+++ src/components/products/products-comp/product-card.js
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 
 import { Button } from "../../../UI/Button/Button";
-import tempPhoto from '../../../assets/img/coffeeTemp.png'
 
 const ProductCard = ({el, handleTempCartAdd}) => {
     const {
@@ -23,7 +22,7 @@ const ProductCard = ({el, handleTempCartAdd}) => {
     return (
         <div className="product-card">
             <div className="product-card-img__container">
-                <img src={image || tempPhoto} className="product-card-img" alt={'Product card'}/>
+                <img src={image} className="product-card-img" alt={'Product card'}/>
             </div>
 
             <div className="product-card-info">
